3bc1e9797f8ec51450e2451839e91e0cce49ba92
Albert Chu <albert.chu@umgc.edu>
2025-12-07 20:52:05 -0500

    All Power to the Soviets!

--- packages/webapp/src/components/App.tsx
+++ packages/webapp/src/components/App.tsx
@@ -1,4 +1,4 @@
-import { GAME_STATE, PLAYER_CLASSES, SIDEBAR_WIDTH_PX } from "../utilities/Constants"
+import { GAME_STATE, PLAYER_CLASSES } from "../utilities/Constants"
 import { useWindowSize } from "../hooks/useWindowSize"
 import { DynamicWebappConfig } from "common"
 import { useRefState } from "../hooks/useRefState"
@@ -17,54 +17,6 @@ import { Banner } from "./Banner"
 
 interface Props {
 	config: DynamicWebappConfig,
-}
-
-// Sidebar component
-function Sidebar() {
-	const navItems = [
-		{ name: "Voting area", icon: "/icons/ballot.jpg", href: "#" },
-		{ name: "Policies area", icon: "/icons/Luxury.svg", href: "#" },
-		{ name: "Open Market", icon: "/icons/Food.svg", href: "#" },
-		{ name: "Bussiness deals", icon: "/icons/Luxury.svg", href: "#" },
-		{ name: "Unemployed workers", icon: "/icons/Luxury.svg", href: "#" }
-	];
-
-	return (
-		<aside
-		style={{
-			width: SIDEBAR_WIDTH_PX,
-			height: "100vh",
-			backgroundColor: "#1f2937", // Tailwind gray-800
-			color: "white",
-			padding: "20px",
-			boxSizing: "border-box",
-			position: "fixed",
-			top: 0,
-			left: 0,
-			zIndex: 1000
-		}}
-		>
-		<h1 style={{ marginBottom: 20 }}>Manifest</h1>
-		{
-			navItems.map((item) => (
-				<a
-					key={item.name}
-					href={item.href}
-					style={{
-					display: "flex",
-					alignItems: "center",
-					gap: "10px",
-					marginBottom: "16px",
-					color: "white",
-					textDecoration: "none"
-				}}>
-					<img src={item.icon} alt={item.name} style={{ width: 24, height: 24 }} />
-					<span>{item.name}</span>
-				</a>
-			))
-		}
-		</aside>
-	);
 }
 
 export function App(props: Props) {
@@ -105,8 +57,7 @@ export function App(props: Props) {
 
 	return (
 		<div style={{display: "flex", userSelect: "none"}}>
-			<Sidebar />
-			<div style={{ marginLeft: "250px", width: "100%" }}>
+			<div style={{ width: "100%" }}>
 				<div style={{display: "flex", alignItems: "center", gap: 10, padding: 5, fontSize: 16}}>
 					<div>Observing as:</div>
 					<RadioSelector choices={PLAYER_CLASSES.map(playerClass => ({value: playerClass.name, content: playerClass.name}))} value={observingAsPlayerClassName.current} onChange={newClassName => observingAsPlayerClassName.current = newClassName as PlayerClassName} radioButtonSize={16}/>
@@ -141,7 +92,7 @@ export function App(props: Props) {
 				}
 				{
 					actionExecution.current !== undefined ? (
-						<div style={{position: "fixed", bottom: 0, left: SIDEBAR_WIDTH_PX, right: 0, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: 20, padding: 20, backgroundColor: getShade(2)}}>
+						<div style={{position: "fixed", bottom: 0, left: 0, right: 0, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: 20, padding: 20, backgroundColor: getShade(2)}}>
 							<span style={{fontSize: "x-large", fontWeight: "bold"}}>{actionExecution.current.action.name}</span>
 							{actionExecution.current.text}
 						</div>
