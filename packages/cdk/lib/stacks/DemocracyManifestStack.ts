da35d1e2fb07dc520fb8ffcc5a16f0524b29c4af
Paul Armstrong <paul83637@gmail.com>
2026-01-18 23:55:40 +0000

    upgrade to node 22, switch from create-react-app to vite

--- packages/cdk/lib/stacks/DemocracyManifestStack.ts
+++ packages/cdk/lib/stacks/DemocracyManifestStack.ts
@@ -88,8 +88,8 @@ export class DemocracyManifestStack extends cdk.Stack {
 		const websiteAsset = new AssetWithBuild(this, "WebsiteAsset", {
 			path: "../webapp",
 			build: (exec, outputDir) => {
-				exec("npx react-scripts build --color=always", {
-					env: { BUILD_PATH: outputDir },
+				exec("npm run build", {
+					env: { BUILD_PATH: outputDir }
 				})
 				exec(`rm -f ${outputDir}/config.json`)
 			},
